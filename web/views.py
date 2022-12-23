aea7daefd10c8d355f2635f871366bf05ad69a01
Dino Wei <dreamer@dcsh.tp.edu.tw>
2022-12-23 10:50:07 +0800

    Msg List, View, Create ... ok

--- web/views.py
+++ web/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Message
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class MessageList(ListView):
+    model = Message
+    odering = ['-id']
+
+class MessageDetail(DetailView):
+    model = Message
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = ['user', 'subject', 'content']
+    success_url = reverse_lazy('msg_list')
